99bb64013845ac72f7d4d154107d9fa142cb30c9
mpombo <m.pombo1@udc.es>
2023-06-07 01:02:24 +0200

    Spinning cube inicial

---
+++ spinningcube_withlight.cpp
@@ -0,0 +1,359 @@
+// Copyright (C) 2020 Emilio J. Padrón
+// Released as Free Software under the X11 License
+// https://spdx.org/licenses/X11.html
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+// GLM library to deal with matrix operations
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::perspective
+#include <glm/gtc/type_ptr.hpp>
+
+#include "textfile_ALT.h"
+
+int gl_width = 640;
+int gl_height = 480;
+
+void glfw_window_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void render(double);
+
+GLuint shader_program = 0; // shader program to set render pipeline
+GLuint vao = 0; // Vertext Array Object to set input data
+GLint model_location, view_location, proj_location; // Uniforms for transformation matrices
+
+// Shader names
+const char *vertexFileName = "spinningcube_withlight_vs.glsl";
+const char *fragmentFileName = "spinningcube_withlight_fs.glsl";
+
+// Camera
+glm::vec3 camera_pos(0.0f, 0.0f, 3.0f);
+
+// Lighting
+glm::vec3 light_pos(1.2f, 1.0f, 2.0f);
+glm::vec3 light_ambient(0.2f, 0.2f, 0.2f);
+glm::vec3 light_diffuse(0.5f, 0.5f, 0.5f);
+glm::vec3 light_specular(1.0f, 1.0f, 1.0f);
+
+// Material
+glm::vec3 material_ambient(1.0f, 0.5f, 0.31f);
+glm::vec3 material_diffuse(1.0f, 0.5f, 0.31f);
+glm::vec3 material_specular(0.5f, 0.5f, 0.5f);
+const GLfloat material_shininess = 32.0f;
+
+int main() {
+  /*
+   * Inicialización e configuración de GLFW
+   */
+  // start GL context and O/S window using the GLFW helper library
+  if (!glfwInit()) {
+    fprintf(stderr, "ERROR: could not start GLFW3\n");
+    return 1;
+  }
+
+  /*
+   * Para cambiar a versión
+   */
+  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  /*
+   * Creación da xanela e o contexto OpenGL
+   */
+  GLFWwindow* window = glfwCreateWindow(gl_width, gl_height, "My spinning cube", NULL, NULL);
+  if (!window) {
+    fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+    glfwTerminate();
+    return 1;
+  }
+  glfwSetWindowSizeCallback(window, glfw_window_size_callback);
+  glfwMakeContextCurrent(window);
+
+  /*
+   * Chamadas a API
+   */
+  // start GLEW extension handler
+  // glewExperimental = GL_TRUE;
+  glewInit();
+
+  // get version info
+  const GLubyte* vendor = glGetString(GL_VENDOR); // get vendor string
+  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+  const GLubyte* glversion = glGetString(GL_VERSION); // version as a string
+  const GLubyte* glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION); // version as a string
+  printf("Vendor: %s\n", vendor);
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", glversion);
+  printf("GLSL version supported %s\n", glslversion);
+  printf("Starting viewport: (width: %d, height: %d)\n", gl_width, gl_height);
+
+  // Enable Depth test: only draw onto a pixel if fragment closer to viewer
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS); // set a smaller value as "closer"
+
+  // Vertex Shader
+  const char* vertex_shader = textFileRead(vertexFileName);
+
+  // Fragment Shader
+  const char* fragment_shader = textFileRead(fragmentFileName);
+
+  // Shaders compilation
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, NULL);
+  free((void*) vertex_shader);
+  glCompileShader(vs);
+
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vs, 512, NULL, infoLog);
+    printf("ERROR: Vertex Shader compilation failed!\n%s\n", infoLog);
+
+    return(1);
+  }
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, NULL);
+  free((void*) fragment_shader);
+  glCompileShader(fs);
+
+  glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fs, 512, NULL, infoLog);
+    printf("ERROR: Fragment Shader compilation failed!\n%s\n", infoLog);
+
+    return(1);
+  }
+
+  // Create program, attach shaders to it and link it
+  shader_program = glCreateProgram();
+  glAttachShader(shader_program, fs);
+  glAttachShader(shader_program, vs);
+  glLinkProgram(shader_program);
+
+  glValidateProgram(shader_program);
+  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+  if(!success) {
+    glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+    printf("ERROR: Shader Program linking failed!\n%s\n", infoLog);
+
+    return(1);
+  }
+
+  // Release shader objects
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  // Vertex Array Object
+  /*
+   * Crear e activar antes dos VBOs e Vertex attributes a agrupar neste VAO
+   */
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // Cube to be rendered
+  //
+  //          0        3
+  //       7        4 <-- top-right-near
+  // bottom
+  // left
+  // far ---> 1        2
+  //       6        5
+  //
+  const GLfloat vertex_positions[] = {
+    -0.25f, -0.25f, -0.25f, // 1
+    -0.25f,  0.25f, -0.25f, // 0
+     0.25f, -0.25f, -0.25f, // 2
+
+     0.25f,  0.25f, -0.25f, // 3
+     0.25f, -0.25f, -0.25f, // 2
+    -0.25f,  0.25f, -0.25f, // 0
+
+     0.25f, -0.25f, -0.25f, // 2
+     0.25f,  0.25f, -0.25f, // 3
+     0.25f, -0.25f,  0.25f, // 5
+
+     0.25f,  0.25f,  0.25f, // 4
+     0.25f, -0.25f,  0.25f, // 5
+     0.25f,  0.25f, -0.25f, // 3
+
+     0.25f, -0.25f,  0.25f, // 5
+     0.25f,  0.25f,  0.25f, // 4
+    -0.25f, -0.25f,  0.25f, // 6
+
+    -0.25f,  0.25f,  0.25f, // 7
+    -0.25f, -0.25f,  0.25f, // 6
+     0.25f,  0.25f,  0.25f, // 4
+
+    -0.25f, -0.25f,  0.25f, // 6
+    -0.25f,  0.25f,  0.25f, // 7
+    -0.25f, -0.25f, -0.25f, // 1
+
+    -0.25f,  0.25f, -0.25f, // 0
+    -0.25f, -0.25f, -0.25f, // 1
+    -0.25f,  0.25f,  0.25f, // 7
+
+     0.25f, -0.25f, -0.25f, // 2
+     0.25f, -0.25f,  0.25f, // 5
+    -0.25f, -0.25f, -0.25f, // 1
+
+    -0.25f, -0.25f,  0.25f, // 6
+    -0.25f, -0.25f, -0.25f, // 1
+     0.25f, -0.25f,  0.25f, // 5
+
+     0.25f,  0.25f,  0.25f, // 4
+     0.25f,  0.25f, -0.25f, // 3
+    -0.25f,  0.25f,  0.25f, // 7
+
+    -0.25f,  0.25f, -0.25f, // 0
+    -0.25f,  0.25f,  0.25f, // 7
+     0.25f,  0.25f, -0.25f  // 3
+  };
+
+// Vertex Buffer Object (for vertex coordinates)
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
+
+  /*
+   * Conectar VBO e entradas do vertex shader
+   */
+  // Vertex attributes
+  // 0: vertex position (x, y, z)
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  // 0: número de vertex attribute en VS (location)
+  // 3, GL_FLOAT: 3 x GL_FLOAT por vértice
+  // GL_FALSE: non queremos que se normalicen estes valores
+  // 0: stride entre elementos
+  // NULL ((void*)0): offset de comezo no VBO
+
+  glEnableVertexAttribArray(0); // Activar atributo, mapeando con IN var en VS 
+
+  // 1: vertex normals (x, y, z)
+
+  // Unbind vbo (it was conveniently registered by VertexAttribPointer)
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // Unbind vao
+  glBindVertexArray(0);
+
+  // Uniforms
+  // - Model matrix
+  // - View matrix
+  // - Projection matrix
+  // - Normal matrix: normal vectors from local to world coordinates
+  // - Camera position
+  // - Light data
+  // - Material data
+
+  /* Localizadores das variables do shader (model, projection, etc) que se usan despois
+    para darlle un valor a ditas variables */
+  model_location = glGetUniformLocation(shader_program, "model");
+  view_location = glGetUniformLocation(shader_program, "view");
+  proj_location = glGetUniformLocation(shader_program, "projection");
+
+  /*
+   * Bucle de render para manter a ventana aberta
+   */
+// Render loop
+  while(!glfwWindowShouldClose(window)) {
+
+    processInput(window);       //Xestionamos entrada de usuario
+
+    render(glfwGetTime());      //Comandos de render para debuxar nun buffer
+
+    glfwSwapBuffers(window);    //Intercambia back e front buffer no framebuffer activo
+
+    glfwPollEvents();           //Procesamos eventos varios
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
+
+void render(double currentTime) {
+  float f = (float)currentTime * 0.3f;
+
+  //Borrado buffers de cor e profundidade
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  //Tamaño do espacio de render
+  glViewport(0, 0, gl_width, gl_height);
+
+  glUseProgram(shader_program);
+  glBindVertexArray(vao);
+
+  glm::mat4 model_matrix, view_matrix, proj_matrix;
+
+  //model_matrix = glm::mat4(1.f);
+  view_matrix = glm::lookAt(                 camera_pos,  // pos
+                            glm::vec3(0.0f, 0.0f, 0.0f),  // target
+                            glm::vec3(0.0f, 1.0f, 0.0f)); // up
+
+  // Moving cube
+  // model_matrix = glm::rotate(model_matrix,
+  //   [...]
+  
+
+  /*  DESPLAZAMIENTO HACIA ATRÁS EN EL EJE Z */
+  //Creamos una matriz de traslación usando la matriz identidad (mat4(1.f)) y
+  //un vector de desplazamiento vec3(0.0f, 0.0f, -4.0f)
+  model_matrix = glm::translate(glm::mat4(1.f), glm::vec3(0.0f, 0.0f, -4.0f));
+  //Añade otra matriz de traslación que varía el desplazamiento con el tiempo
+  model_matrix = glm::translate(model_matrix,
+                             glm::vec3(sinf(2.1f * f) * 0.5f,
+                                       cosf(1.7f * f) * 0.5f,
+                                       sinf(1.3f * f) * cosf(1.5f * f) * 2.0f));
+
+  /*  ROTACIÓN  */
+  // Matriz de rotación que gira en torno al eje "y" (0.0f, 1.0f, 0.0f) 
+  // y el ángulo de rotación se calcula en función al tiempo actual
+  model_matrix = glm::rotate(model_matrix,
+                          glm::radians((float)currentTime * 45.0f),
+                          glm::vec3(0.0f, 1.0f, 0.0f));
+                          
+  // Matriz de rotación que gira en torno al eje "x" (1.0f, 0.0f, 0.0f) 
+  // y el ángulo de rotación se calcula en función al tiempo actual
+  model_matrix = glm::rotate(model_matrix,
+                          glm::radians((float)currentTime * 81.0f),
+                          glm::vec3(1.0f, 0.0f, 0.0f));
+
+
+  //Establecemos el valor para model_matrix en el shader usando su localizador
+  glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(model_matrix));
+
+
+  // Projection
+  // proj_matrix = glm::perspective(glm::radians(50.0f),
+  //   [...]
+  proj_matrix = glm::perspective(glm::radians(50.0f),
+                                 (float) gl_width / (float) gl_height,
+                                 0.1f, 1000.0f);
+
+  //Establecemos el valor para proj_matrix en el shader usando su localizador
+  glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(proj_matrix));
+
+  // Normal matrix: normal vectors to world coordinates
+
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+void processInput(GLFWwindow *window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, 1);
+}
+
+// Callback function to track window size and update viewport
+void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
+  gl_width = width;
+  gl_height = height;
+  printf("New viewport: (width: %d, height: %d)\n", width, height);
+}
